e406c0c788044b19470744471ef1cbe06bdcbdcb
Orhan Türk <orhan.turk21@gmail.com>
2016-09-20 16:34:25 +0300

    Add files via upload

---
+++ ComputerService/ComputerService/Library.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using Npgsql;
+using System.Net;
+
+namespace ComputerService
+{
+    public static class Library
+    {
+        private static string server = "192.168.2.6";
+
+        private static string database = "servicecontrol";
+
+        private static string username = "servicecontrol";
+
+        private static string password = "mokdakm";
+
+
+        public static string SqlStr = "Server=" + server + ";Port=5432;Database=" + database + ";User Id=" + username + ";Password=" + password + ";Pooling=false;";
+
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Kayit.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Kayit.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+        public static void NewRegister()
+        {
+            string username = GetComputerName();
+
+            string ipadress = GetIpAddress();
+
+            if (username.Length > 2)
+            {
+                int _regcontrol = NewRegisterControl(username);
+
+                if (_regcontrol == 0)
+                {
+                    using (NpgsqlConnection cnn = new NpgsqlConnection(SqlStr))
+                    {
+                        cnn.Open();
+                        NpgsqlCommand cmd = new NpgsqlCommand("insert into t_user (ip_adres,username) values(@ip_adres,@username)", cnn);
+                        cmd.Parameters.AddWithValue("@ip_adres", ipadress);
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.ExecuteNonQuery();
+                        cnn.Close();
+                    }
+                }
+            }
+        }
+
+        public static int NewRegisterControl(string username)
+        {
+            int result = 0;
+
+            using (NpgsqlConnection cnn = new NpgsqlConnection(SqlStr))
+            {
+                cnn.Open();
+                try
+                {
+                    NpgsqlCommand emir = new NpgsqlCommand("select id from t_user where username='" + username + "'", cnn);
+                    result = Convert.ToInt32(emir.ExecuteScalar());
+                }
+                catch { }
+                cnn.Close();
+            }
+            return result;
+
+        }
+
+        public static string CommandControl(string username)
+        {
+            string result = "";
+            using (NpgsqlConnection cnn = new NpgsqlConnection(SqlStr))
+            {
+                cnn.Open();
+                try
+                {
+                    NpgsqlCommand emir = new NpgsqlCommand("select command from t_command where username='" + username + "' and statu=true", cnn);
+                    result = emir.ExecuteScalar().ToString();
+                }
+                catch { }
+                cnn.Close();
+            }
+            return result;
+        }
+
+        public static void DeleteCommand(string username)
+        {
+            using (NpgsqlConnection cnn = new NpgsqlConnection(SqlStr))
+            {
+                cnn.Open();
+                try
+                {
+                    NpgsqlCommand emir = new NpgsqlCommand("delete from t_command where username='" + username + "' and statu=true", cnn);
+                    emir.ExecuteNonQuery().ToString();
+                }
+                catch { }
+                cnn.Close();
+            }
+        }
+        public static string GetComputerName()
+        {
+
+            string domainName = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+            string hostName = Dns.GetHostName();
+            string fqdn = "";
+            if (!hostName.Contains(domainName))
+                fqdn = hostName + "." + domainName;
+            else
+                fqdn = hostName;
+
+            return fqdn;
+        }
+
+        public static string GetIpAddress()
+        {
+            string hostName = Dns.GetHostName();
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+#pragma warning restore CS0618 // Type or member is obsolete
+            return myIP;
+        }
+    }
+}
---
+++ ComputerService/ComputerService/Scheduler.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace ComputerService
+{
+    public partial class Scheduler : ServiceBase
+    {
+        [DllImport("user32")]
+        public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+        private Timer timer1 = null;
+
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            switch (changeDescription.Reason)
+            {
+                case SessionChangeReason.SessionLogon:
+                    Library.WriteErrorLog("Bilgisayar Açılış Saati: \t " + DateTime.Now);
+                    break;
+                case SessionChangeReason.SessionLogoff:
+                    Library.WriteErrorLog("Bilgisayar Kapanış Saati: \t " + DateTime.Now);
+                    break;
+                case SessionChangeReason.RemoteConnect:
+                    Library.WriteErrorLog("Uzak Bağlantı Açılış Saati: \t " + DateTime.Now);
+                    break;
+                case SessionChangeReason.RemoteDisconnect:
+                    Library.WriteErrorLog("Uzak Bağlantı Kapanış Saati: \t " + DateTime.Now);
+                    break;
+                case SessionChangeReason.SessionLock:
+                    Library.WriteErrorLog("Oturum Kilitleme Saati: \t" + DateTime.Now);
+                    break;
+                case SessionChangeReason.SessionUnlock:
+                    Library.WriteErrorLog("Oturum Açma Saati: \t " + DateTime.Now);
+                    break;
+                default:
+                    break;
+            }
+        }
+        protected override void OnStart(string[] args)
+        {
+
+            timer1 = new Timer();
+            this.timer1.Interval = 30000; //every 30 secs
+            this.timer1.Elapsed += new ElapsedEventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+
+
+            Library.WriteErrorLog("Servis Başladı");
+
+        }
+
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            string _username = Library.GetComputerName();
+
+            string _ipadres = Library.GetIpAddress();
+
+            Library.NewRegister();
+
+            string _command = Library.CommandControl(_username);
+
+            if (_command.Length > 2)
+            {
+                DoOperation(_command);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Library.WriteErrorLog("Servis Durduruldu");
+        }
+        private void DoOperation(string oparation)
+        {
+            string filename = string.Empty;
+            string arguments = string.Empty;
+            string _username = Library.GetComputerName();
+            switch (oparation)
+            {
+                case "ShutDown":
+                    Library.DeleteCommand(_username);
+                    Library.WriteErrorLog("Kapat Komutu Çalıştırıldı");
+                    var shutdown = new ProcessStartInfo("shutdown", "/s /t 0");
+                    shutdown.CreateNoWindow = true;
+                    shutdown.UseShellExecute = false;
+                    Process.Start(shutdown);
+                    //  filename = "shutdown.exe";
+                    //arguments = "-s";
+                    break;
+                case "Restart":
+                    Library.DeleteCommand(_username);
+                    Library.WriteErrorLog("Yeniden Başlat Komutu Çalıştırıldı");
+                    var rebbbot = new ProcessStartInfo("shutdown", "/r /t 0");
+                    rebbbot.CreateNoWindow = true;
+                    rebbbot.UseShellExecute = false;
+                    Process.Start(rebbbot);
+                    //filename = "shutdown.exe";
+                    //arguments = "-r";
+                    break;
+
+                    /*
+                case "Logoff":
+                    Library.WriteErrorLog("Oturum Kapat Komutu Çalıştırıldı");
+                    ExitWindowsEx(0, 0);
+                    // filename = "shutdown.exe";
+                    //arguments = "-l";
+                    break;
+                case "Lock":
+                    Library.WriteErrorLog("Kitleme Komutu Çalıştırıldı");
+                    //LockWorkStation();
+                    //filename = "Rundll32.exe";
+                    //arguments = "User32.dll, LockWorkStation";
+                    break;
+                    /*
+                case "Hibernation":
+                    Library.WriteErrorLog("Bekleme Modu Komutu Çalıştırıldı");
+                    filename = @"%windir%\system32\rundll32.exe";
+                    arguments = "PowrProf.dll, SetSuspendState";
+                    break;
+                case "Sleep":
+                    Library.WriteErrorLog("Uyku Modu Komutu Çalıştırıldı");
+                    filename = "Rundll32.exe";
+                    arguments = "powrprof.dll, SetSuspendState 0,1,0";
+                    break;
+                    */
+            }
+            /*
+            ProcessStartInfo startinfo = new ProcessStartInfo(filename, arguments);
+            Process.Start(startinfo);
+            */
+        }
+    }
+}
